5a0f914b51fba20cf66261cb7dcce51d58e7d73b
thisisstreamliner <thisisstreamliner@gmail.com>
2020-11-08 16:09:51 +0200

    Create routes, custom bottom navigation bar, themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'package:clone_e_commerce_app/src/config/route.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'src/pages/main_page.dart';
+import 'src/pages/product_detail.dart';
+import 'src/themes/theme.dart';
+import 'src/widgets/custom_route.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +16,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'E-Commerce App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'E-Commerce',
+      theme: AppTheme.lightTheme.copyWith(
+        textTheme: GoogleFonts.muliTextTheme(
+          Theme.of(context).textTheme,
+        ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      routes: Routes.getRoute(),
+      onGenerateRoute: (RouteSettings settings) {
+        if (settings.name.contains('detail')) {
+          return CustomRoute<bool>(
+              builder: (BuildContext context) => ProductDetailPage());
+        } else {
+          return CustomRoute<bool>(
+              builder: (BuildContext context) => MainPage());
+        }
+      },
+      initialRoute: "MainPage",
     );
   }
 }
---
+++ lib/src/pages/shopping_cart_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ShoppingCartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('ShoppingCartPage'),
+      ),
+    );
+  }
+}
